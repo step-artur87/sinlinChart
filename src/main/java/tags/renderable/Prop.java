b057776c85792029099f12cae0b4c19b7814865f
art <step.artur87@gmail.com>
2015-09-07 22:37:29 +0300

    fixed bug

--- src/main/java/tags/renderable/Prop.java
+++ src/main/java/tags/renderable/Prop.java
@@ -19,19 +19,22 @@ import java.util.ArrayList;
 public class Prop extends AbstractRenderTag implements Tag {
     private ArrayDeque<Fn> fnSet = new ArrayDeque<>();//fixme kostyl , todo scale
 
-    protected void setRenderInstruction(){
+    protected void setRenderInstruction() {
         ArrayList<Double> doubleArrayList;
         String value = getAttributes().getValue(
                 Tag.TYPE);
-        if ((value.equals(RenderInstruction.COLOR))) {
-            doubleArrayList =
-                    Data.getInstance().getDataMap().get(
-                            value);
-            renderInstruction = new D1RI(
-                    value,
-                    getAttributes().getValue(
-                            RenderInstruction.LEGEND),
-                    doubleArrayList);
+        if (!(getAttributes().getValue(
+                Tag.TYPE) == null)) {
+            if ((value.equals(RenderInstruction.COLOR))) {
+                doubleArrayList =
+                        Data.getInstance().getDataMap().get(
+                                value);
+                renderInstruction = new D1RI(
+                        value,
+                        getAttributes().getValue(
+                                RenderInstruction.LEGEND),
+                        doubleArrayList);
+            }
         }
     }
 
