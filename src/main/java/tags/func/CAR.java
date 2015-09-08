91479f6345f580c967c82298e9948f2ed47e6349
art <step.artur87@gmail.com>
2015-09-08 10:09:50 +0300

    addChildTag() there check id unical

--- src/main/java/tags/func/CAR.java
+++ src/main/java/tags/func/CAR.java
@@ -3,8 +3,11 @@ package tags.func;
 import tags.AbstractTag;
 import tags.Tag;
 import tags.TagException;
+import xml.DataSourceHandler;
 
 import java.util.ArrayDeque;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,10 +17,35 @@ import java.util.ArrayDeque;
  */
 public class CAR extends AbstractTag {
     private ArrayDeque<Num> nums = new ArrayDeque<>();
+    //todo choose type of collection for all
+    private Set<String> ids = new HashSet<>();
 
+    /**
+     * Add specified tag to this tag if this tag can have
+     * this class as child (this tag has not "id"
+     * or its id unical)
+     * and throws TagException otherwise.
+     *
+     * @param tag that becomes a child of this if can
+     * @throws TagException
+     */
     @Override
     public void addChildTag(Tag tag) throws TagException {
-        if (tryAddToArray(tag, Num.class, getNums()))
+        String value;
+        if (tag.getAttributes() != null) {
+            value = tag.getAttributes().getValue(
+                    DataSourceHandler.ID);
+            if (!getIds().contains(value)) {
+                if (tryAddToArray(tag, Num.class, getNums())){
+                    getIds().add(value);
+                    return;
+                }
+            } else {
+                throw new TagException("Tag with id "
+                        + value
+                        + " already present");
+            }
+        } else if (tryAddToArray(tag, Num.class, getNums()))
             return;
 
         throw new TagException("Can not add "
@@ -28,5 +56,9 @@ public class CAR extends AbstractTag {
 
     public ArrayDeque<Num> getNums() {
         return nums;
+    }
+
+    public Set<String> getIds() {
+        return ids;
     }
 }
