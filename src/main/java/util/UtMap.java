ba941bdb5dd2faa9e4ec322a34bf6085181b25ce
art <step.artur87@gmail.com>
2015-09-13 15:31:06 +0300

    tmp

--- src/main/java/util/UtMap.java
+++ src/main/java/util/UtMap.java
@@ -1,8 +1,10 @@
 package util;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.OptionalInt;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,18 +15,45 @@ import java.util.stream.Stream;
  */
 public class UtMap {
     public static Map<String, ArrayList<Double>> getRows(
-            Map<String, ArrayList<Double>> data){
+            Map<String, ArrayList<Double>> data) {
         Map<String, ArrayList<Double>> rows
                 = new HashMap<>();
 
+        int n = getMapArrayListsSize(data);
+        if (n > 0) {
+            for (int i = 0; i < n; i++) {
+
+            }
+        } else {
+            //todo
+        }
+        return rows;
     }
 
-    public static int getMapArrayListsSize(Map<String, ArrayList<Double>>map){
-        IntStream intStream = map.values().stream().mapToInt((a)-> a.size());
+    public static int getMapArrayListsSize(Map<String, ArrayList<Double>> map) {
+        IntStream intStream = map.values().stream().mapToInt((a) -> a.size());
         OptionalInt max = intStream.max();
         OptionalInt min = intStream.max();
         if (!max.isPresent() && !min.isPresent()) return 0;
         if (max == min) return max.getAsInt();
         return -1;
+    }
+
+    //without checking
+    public static ArrayList<ArrayList<Double>> transpose(
+            ArrayList<ArrayList<Double>> data){
+        ArrayList<ArrayList<Double>> result = new ArrayList<>();
+        int w = data.size();
+        int h = data.get(0).size();
+
+        for (int i = 0; i < h; i++) {
+            result.add(new ArrayList<>());
+        }
+        for (int i = 0; i < w; i++) {
+            for (int j = 0; j < h; j++) {
+                //result.
+            }     
+        }
+        return result;
     }
 }
